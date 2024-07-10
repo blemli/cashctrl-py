9301f1620084283c179f56cad3dba761c7ad770f
grafst <grafst@users.noreply.github.com>
2024-07-10 11:21:33 +0200

    v 0.0.13

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
-python3 -m pip install --upgrade twine
-python3 -m pip install --upgrade build
+uv venv
+source .venv/bin/activate
+uv pip install --upgrade twine
+uv pip install --upgrade build
 rm -rf dist/*
 python3 -m build
 python3 -m twine upload --repository pypi dist/* --user __token__
